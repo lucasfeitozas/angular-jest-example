dadad7abc69678ad2e0429d89391e0cceb720881
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-05-23 14:29:57 -0300

    edicao README.md

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+// @lucas.af
+// https://www.amadousall.com/how-to-set-up-angular-unit-testing-with-jest/
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
